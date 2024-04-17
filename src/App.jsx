43e90ff1abb6e3d14f5c09a9cfcb699adcb375f0
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-04-17 22:15:56 +0530

    locomotive added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import Stripes from './Components/Stripes'
 import Products from './Components/Products'
 import Marquees from './Components/Marquees'
 import Cards from './Components/Cards'
+import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className='bg-[#161618] w-full   text-white'>
         <Navbar/>
